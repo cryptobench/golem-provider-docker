d7fac9ee84a8bff3c433eb635195ef53fa67a437
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-09-19 22:09:00 +0200

    Init

---
+++ init.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import json
+import os
+
+if __name__ == "__main__":
+    # generate globals file
+    parent_folder = "~/.local/share/ya-provider/"
+    globals_path = "~/.local/share/ya-provider/globals.json"
+    hardware_path = "~/.local/share/ya-provider/hardware.json"
+    presets_path = "~/.local/share/ya-provider/presets.json"
+
+    if not os.path.exists(parent_folder):
+        os.mkdir(parent_folder)
+
+    with open(globals_path, "w") as f:
+        data = {
+            "node_name": os.environ.get("NODE_NAME", "docker-provider"),
+            "subnet": os.environ.get("NODE_SUBNET", "public-beta"),
+            "account": os.environ.get("YA_ACCOUNT", "null"),
+        }
+        json.dump(data, f, indent=4)
+
+    with open(hardware_path, "w") as f:
+        data = {
+            "active": "default",
+            "profiles": {
+                "default": {
+                    "cpu_threads": int(os.environ.get("NODE_CPU_THREADS", "null")),
+                    "mem_gib": float(os.environ.get("NODE_MEM_GIB", "null")),
+                    "storage_gib": float(os.environ.get("NODE_STORAGE_GIB", "null")),
+                }
+            },
+        }
+        json.dump(data, f, indent=4)
+
+    # seems like lots of copy and paste but I want to keep it flexible
+    with open(presets_path, "w") as f:
+        data = {
+            "active": ["wasmtime", "vm"],
+            "presets": [
+                {
+                    "name": "default",
+                    "exeunit-name": "wasmtime",
+                    "pricing-model": "linear",
+                    "usage-coeffs": {
+                        "initial": float(os.environ.get("NODE_COSTS_START", 0)),
+                        "duration": float(os.environ.get("NODE_COSTS_HOUR", 0.02))
+                        / 3600,
+                        "cpu": float(os.environ.get("NODE_COSTS_CPU_HOUR", 0.1)) / 3600,
+                    },
+                },
+                {
+                    "name": "vm",
+                    "exeunit-name": "vm",
+                    "pricing-model": "linear",
+                    "usage-coeffs": {
+                        "initial": float(os.environ.get("NODE_COSTS_START", 0)),
+                        "duration": float(os.environ.get("NODE_COSTS_HOUR", 0.02))
+                        / 3600,
+                        "cpu": float(os.environ.get("NODE_COSTS_CPU_HOUR", 0.1)) / 3600,
+                    },
+                },
+                {
+                    "name": "wasmtime",
+                    "exeunit-name": "wasmtime",
+                    "pricing-model": "linear",
+                    "usage-coeffs": {
+                        "initial": float(os.environ.get("NODE_COSTS_START", 0)),
+                        "duration": float(os.environ.get("NODE_COSTS_HOUR", 0.02))
+                        / 3600,
+                        "cpu": float(os.environ.get("NODE_COSTS_CPU_HOUR", 0.1)) / 3600,
+                    },
+                },
+            ],
+        }
+        json.dump(data, f, indent=4)
+
+        print("Node settings generated!")
